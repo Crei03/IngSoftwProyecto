7a8b3b9415211b382fa64c0dfa8da1af755e4191
crei03 <carlosreina2100@gmail.com>
2025-07-26 12:28:50 -0500

    migracion a springboot

---
+++ src/main/java/com/seguratuauto/SeguratuautoApplication.java
@@ -0,0 +1,11 @@
+package com.seguratuauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguratuautoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeguratuautoApplication.class, args);
+    }
+}
